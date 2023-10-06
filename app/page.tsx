3dd596a2699170c63f8dce1c3e2d87d4209d3e36
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2023-10-06 09:03:12 +0300

    Configured ESLint, Prettier and extended rules with TailwindCSS

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
-        <div className="flex max-xl:flex-col gap-16">
+      <section className="border-2 border-red-500 px-6 py-24 md:px-20">
+        <div className="flex gap-16 max-xl:flex-col">
           <div className="flex flex-col justify-center">
             <p className="small-text">
               Smart Shopping Starts Here:
